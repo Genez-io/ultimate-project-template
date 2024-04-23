8df4e2d809ca456c6b3d6b3366ee2edc46c817e7
Virgil993 <turcu.virgil@gmail.com>
2024-04-23 11:02:02 +0300

    deleted all contents

--- README.md
+++
@@ -1,2 +0,0 @@
-# ultimate-project-template
-A template for how to structure large backend projects with Genezio
--- utils/helperFunction.ts
+++
@@ -1,7 +0,0 @@
-import { Task } from "../db/mongooseModel";
-
-export function filterTasksByDate(tasks: Task[], date: Date) {
-  return tasks.filter(
-    (task) => task.date.toDateString() === date.toDateString()
-  );
-}
